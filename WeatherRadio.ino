11e1f4179fac7639431015ade7f1008b453a50c4
Derek Chafin <infomaniac50@gmail.com>
2014-05-10 19:08:22 -0500

    Updates

--- WeatherRadio.ino
+++ WeatherRadio.ino
@@ -21,6 +21,9 @@ struct StoreStruct
 
 } storage;
 
+tmElements_t tm;
+File log;
+
 void setup()
 {
   // Setup EEPROM with defaults
@@ -34,14 +37,41 @@ void setup()
     saveConfig();
   }
 
+  // Setup Clock
+  if (!RTC.read(tm))
+    loopError();
+
+  // Setup SD Card
+  pinMode(10, OUTPUT);
+
   // Setup Radio
 
+
   // Setup Other Stuff
+
 }
 
 void loop()
 {
 
+}
+
+void loopError()
+{
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  // Blink forever
+  while (true)
+  {
+    delay(1000);
+    digitalWrite(13, HIGH);
+    delay(250);
+    digitalWrite(13, LOW);
+    delay(250);
+    digitalWrite(13, HIGH);
+    delay(250);
+    digitalWrite(13, LOW);
+  }
 }
 
 bool loadConfig()
